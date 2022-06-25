f594e9a4fbcf93570dafcd02bed8fc221b4dba50
podikris <54362616+podikris@users.noreply.github.com>
2022-06-25 08:21:49 +0530

    20220625.2 Added CardTransaction component

---
+++ src/components/CardTransaction.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="avoidable"></div>
+  <div class="left-panel">
+    <span class="amount">{{ data.amount }}</span>
+    <span class="date">{{ data.date }}</span>
+  </div>
+  <div class="right-panel">
+    <span class="purpose">{{ data.purpose }}</span>
+    <span class="payment-mode">{{ data.mode }}</span>
+    <span class="beneficiary">{{ data.benefits }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardTransaction",
+  props: {
+    data: Object,
+  },
+};
+</script>
